59c358b45f1478692b45be808c4dd6fb1e5a4d08
burak <byrkbrk@gmail.com>
2024-03-06 16:13:05 +0300

    add checkpoint-name argument to parser

--- train.py
+++ train.py
@@ -8,11 +8,13 @@ if __name__=="__main__":
     parser.add_argument("--dataset-name", type=str, default="mnist", help="Dataset name for training")
     parser.add_argument("--device", type=str, default="cuda", help="Device for training")
     parser.add_argument("--lrate", type=float, default=1e-3, help="Learning rate for training")
+    parser.add_argument("--checkpoint-name", type=str, default=None, help="Checkpoint name for pre-training")
     args = parser.parse_args()
+    print(args.checkpoint_name, type(args.checkpoint_name))
 
     train_model = TrainModel(batch_size=args.batch_size, n_epoch=args.epochs, 
                              device=args.device, dataset_name=args.dataset_name,
-                             lrate=args.lrate)
+                             lrate=args.lrate, checkpoint_name=args.checkpoint_name)
     train_model.train()
 
 
