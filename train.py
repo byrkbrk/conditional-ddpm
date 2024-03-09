3d2e40a616153b20a5d3bd753945e9c054ab106e
burak <byrkbrk@gmail.com>
2024-03-09 17:31:42 +0300

    update arguments

--- train.py
+++ train.py
@@ -7,13 +7,12 @@ if __name__=="__main__":
     parser.add_argument("--epochs", type=int, default=32, help="Number of epochs for training")
     parser.add_argument("--dataset-name", type=str, default="mnist", help="Dataset name for training")
     parser.add_argument("--device", type=str, default="cuda", help="Device for training")
-    parser.add_argument("--lrate", type=float, default=1e-3, help="Learning rate for training")
+    parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate for training")
     parser.add_argument("--checkpoint-name", type=str, default=None, help="Checkpoint name for pre-training")
     args = parser.parse_args()
 
-    train_model = TrainModel(batch_size=args.batch_size, n_epoch=args.epochs, 
-                             device=args.device, dataset_name=args.dataset_name,
-                             lrate=args.lrate, checkpoint_name=args.checkpoint_name)
-    train_model.train()
+    train_model = TrainModel(device=args.device, dataset_name=args.dataset_name, 
+                             checkpoint_name=args.checkpoint_name)
+    train_model.train(batch_size=args.batch_size, n_epoch=args.epochs, lr=args.lr)
 
 
