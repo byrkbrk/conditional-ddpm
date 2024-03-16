e8ce7bc3a3afa53179df33653695d8431406d408
burak <byrkbrk@gmail.com>
2024-03-16 16:33:08 +0300

    update file

--- sample.py
+++ sample.py
@@ -1,30 +1,26 @@
-from sampling_functions import sample_ddpm
 from torchvision.utils import save_image
 from train_model import TrainModel
-from utils import generate_animation
-import torch
+from utils import generate_animation, get_custom_context
 import os
 import argparse
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Sample images from diffusion model")
     parser.add_argument("--checkpoint-name", type=str, default="checkpoint_0.pth", help="Checkpoint name of diffusion model")
-    parser.add_argument("--n-sample", type=int, default=64, help="Number of samples to generate")
+    parser.add_argument("--n-samples", type=int, default=100, help="Number of samples to generate")
     parser.add_argument("--device", type=str, default="cuda", help="GPU device to use")
+    parser.add_argument("--n-images-per-row", type=int, default=10, help="Number of images each row contains in the grid")
     args = parser.parse_args()
     
     os.makedirs(os.path.join(os.path.dirname(__file__), "generated-images"), exist_ok=True)
     
-    checkpoint = torch.load(os.path.join(os.path.dirname(__file__), "checkpoints", args.checkpoint_name), 
-                            map_location=torch.device(args.device))
-    nn_model = TrainModel(dataset_name=checkpoint["dataset_name"], 
-                          checkpoint_name=args.checkpoint_name, device=args.device).nn_model.eval()
+    train_model = TrainModel(device=args.device, checkpoint_name=args.checkpoint_name)
+    c = get_custom_context(n_samples=args.n_samples, n_classes=train_model.nn_model.n_cfeat, device=train_model.device)
+    samples, intermediate_ddpm = train_model.sample_ddpm(args.n_samples, c)
     
-    samples, intermediate_ddpm = sample_ddpm(n_sample=args.n_sample, n_channel=nn_model.in_channels,
-                                             height=nn_model.height, width=nn_model.width, 
-                                             nn_model=nn_model, timesteps=checkpoint["timesteps"],                                              
-                                             a_t=checkpoint["a_t"], b_t=checkpoint["b_t"], 
-                                             ab_t=checkpoint["ab_t"], device=args.device)
-    generate_animation(intermediate_ddpm, os.path.join(os.path.dirname(__file__), "generated-images"))
-    save_image(samples, os.path.join(os.path.dirname(__file__), "generated-images", "ddpm_images.jpeg"), 
-                scale_each=True, normalize=True)
+    save_image(samples, os.path.join(os.path.dirname(__file__), "generated-images", 
+                                    f"{train_model.dataset_name}_ddpm_images.jpeg"), 
+                                    scale_each=True, normalize=True, nrow=args.n_images_per_row)
+    generate_animation(intermediate_ddpm, os.path.join(os.path.dirname(__file__), "generated-images",
+                                                       f"{train_model.dataset_name}_ani.gif"), 
+                                                       n_images_per_row=args.n_images_per_row)
