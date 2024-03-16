31758d8dda937a40362fea54bd92d5a220ba599a
burak <byrkbrk@gmail.com>
2024-03-16 16:40:41 +0300

    update file

--- models.py
+++ models.py
@@ -1,213 +1,163 @@
 import torch
-import torch.nn as nn
-
+from torch import nn
 
 
 class ResidualConvBlock(nn.Module):
-    def __init__(
-        self, in_channels: int, out_channels: int, is_res: bool = False
-    ) -> None:
-        super().__init__()
-
-        # Check if input and output channels are the same for the residual connection
-        self.same_channels = in_channels == out_channels
-
-        # Flag for whether or not to use residual connection
+    def __init__(self, in_channels, out_channels, is_res=False):
+        super(ResidualConvBlock, self).__init__()
         self.is_res = is_res
-
-        # First convolutional layer
+        self.same_channels = in_channels == out_channels
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1),   # 3x3 kernel with stride 1 and padding 1
-            nn.BatchNorm2d(out_channels),   # Batch normalization
-            nn.GELU(),   # GELU activation function
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU()
         )
-
-        # Second convolutional layer
         self.conv2 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1),   # 3x3 kernel with stride 1 and padding 1
-            nn.BatchNorm2d(out_channels),   # Batch normalization
-            nn.GELU(),   # GELU activation function
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU()
         )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-
-        # If using residual connection
-        if self.is_res:
-            # Apply first convolutional layer
-            x1 = self.conv1(x)
-
-            # Apply second convolutional layer
-            x2 = self.conv2(x1)
-
-            # If input and output channels are the same, add residual connection directly
-            if self.same_channels:
-                out = x + x2
-            else:
-                # If not, apply a 1x1 convolutional layer to match dimensions before adding residual connection
-                shortcut = nn.Conv2d(x.shape[1], x2.shape[1], kernel_size=1, stride=1, padding=0).to(x.device)
-                out = shortcut(x) + x2
-            #print(f"resconv forward: x {x.shape}, x1 {x1.shape}, x2 {x2.shape}, out {out.shape}")
-
-            # Normalize output tensor
-            return out / 1.414
-
-        # If not using residual connection, return output of second convolutional layer
-        else:
-            x1 = self.conv1(x)
-            x2 = self.conv2(x1)
-            return x2
-
-    # Method to get the number of output channels for this block
-    def get_out_channels(self):
-        return self.conv2[0].out_channels
-
-    # Method to set the number of output channels for this block
-    def set_out_channels(self, out_channels):
-        self.conv1[0].out_channels = out_channels
-        self.conv2[0].in_channels = out_channels
-        self.conv2[0].out_channels = out_channels
         
-
-class UnetUp(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(UnetUp, self).__init__()
+        self.handle_channel = None
+        if self.is_res and not self.same_channels:
+            self.handle_channel = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
         
-        # Create a list of layers for the upsampling block
-        # The block consists of a ConvTranspose2d layer for upsampling, followed by two ResidualConvBlock layers
-        layers = [
-            nn.ConvTranspose2d(in_channels, out_channels, 2, 2),
-            ResidualConvBlock(out_channels, out_channels),
-            ResidualConvBlock(out_channels, out_channels),
-        ]
-        
-        # Use the layers to create a sequential model
-        self.model = nn.Sequential(*layers)
-
-    def forward(self, x, skip):
-        # Concatenate the input tensor x with the skip connection tensor along the channel dimension
-        x = torch.cat((x, skip), 1)
-        
-        # Pass the concatenated tensor through the sequential model and return the output
-        x = self.model(x)
-        return x
-
-    
-class UnetDown(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(UnetDown, self).__init__()
-        
-        # Create a list of layers for the downsampling block
-        # Each block consists of two ResidualConvBlock layers, followed by a MaxPool2d layer for downsampling
-        layers = [ResidualConvBlock(in_channels, out_channels), ResidualConvBlock(out_channels, out_channels), nn.MaxPool2d(2)]
-        
-        # Use the layers to create a sequential model
-        self.model = nn.Sequential(*layers)
-
     def forward(self, x):
-        # Pass the input through the sequential model and return the output
+        x_out = self.conv2(self.conv1(x))
+        if not self.is_res:
+            return x_out
+        else:
+            if self.handle_channel:
+                x_out = x_out + self.handle_channel(x)
+            else:
+                x_out = x_out + x
+            return x_out / 1.414 # normalize
+
+
+class UNetDown(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNetDown, self).__init__()
+        layers = [ResidualConvBlock(in_channels, out_channels), # no skip-connection
+                  ResidualConvBlock(out_channels, out_channels),
+                  nn.MaxPool2d(2)]
+        self.model = nn.Sequential(*layers)
+    
+    def forward(self, x):
         return self.model(x)
+
+
+class UNetUp(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNetUp, self).__init__()
+        layers = [
+            nn.ConvTranspose2d(in_channels, out_channels, 2, 2), # double h & w sizes
+            ResidualConvBlock(out_channels, out_channels), # no residual-connection
+            ResidualConvBlock(out_channels, out_channels)
+        ]
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x, x_skip):
+        return self.model(torch.cat([x, x_skip], dim=1))
 
 
 class EmbedFC(nn.Module):
-    def __init__(self, input_dim, emb_dim):
+    def __init__(self, input_dim, hidden_dim):
         super(EmbedFC, self).__init__()
-        '''
-        This class defines a generic one layer feed-forward neural network for embedding input data of
-        dimensionality input_dim to an embedding space of dimensionality emb_dim.
-        '''
         self.input_dim = input_dim
-        
-        # define the layers for the network
         layers = [
-            nn.Linear(input_dim, emb_dim),
+            nn.Linear(input_dim, hidden_dim),
             nn.GELU(),
-            nn.Linear(emb_dim, emb_dim),
+            nn.Linear(hidden_dim, hidden_dim)
         ]
-        
-        # create a PyTorch sequential model consisting of the defined layers
         self.model = nn.Sequential(*layers)
 
     def forward(self, x):
-        # flatten the input tensor
-        x = x.view(-1, self.input_dim)
-        # apply the model layers to the flattened tensor
-        return self.model(x)
+        x = x.reshape(-1, self.input_dim)
+        return self.model(x)[:, :, None, None]
 
 
 class ContextUnet(nn.Module):
-    def __init__(self, in_channels, n_feat=256, n_cfeat=10, height=28):  # cfeat - context features
+    def __init__(self, in_channels, height, width, n_feat, n_cfeat):
         super(ContextUnet, self).__init__()
-
-        # number of input channels, number of intermediate feature maps and number of classes
         self.in_channels = in_channels
+        self.height = height
+        self.width = width
         self.n_feat = n_feat
         self.n_cfeat = n_cfeat
-        self.h = height  #assume h == w. must be divisible by 4, so 28,24,20,16...
 
-        # Initialize the initial convolutional layer
-        self.init_conv = ResidualConvBlock(in_channels, n_feat, is_res=True)
-
-        # Initialize the down-sampling path of the U-Net with two levels
-        self.down1 = UnetDown(n_feat, n_feat)        # down1 #[10, 256, 8, 8]
-        self.down2 = UnetDown(n_feat, 2 * n_feat)    # down2 #[10, 256, 4,  4]
-        
-         # original: self.to_vec = nn.Sequential(nn.AvgPool2d(7), nn.GELU())
-        self.to_vec = nn.Sequential(nn.AvgPool2d((4)), nn.GELU())
-
-        # Embed the timestep and context labels with a one-layer fully connected neural network
-        self.timeembed1 = EmbedFC(1, 2*n_feat)
-        self.timeembed2 = EmbedFC(1, 1*n_feat)
-        self.contextembed1 = EmbedFC(n_cfeat, 2*n_feat)
-        self.contextembed2 = EmbedFC(n_cfeat, 1*n_feat)
-
-        # Initialize the up-sampling path of the U-Net with three levels
+        self.init_conv = ResidualConvBlock(in_channels, n_feat, True)
+        self.down1 = UNetDown(n_feat, n_feat)
+        self.down2 = UNetDown(n_feat, 2*n_feat)
+        self.to_vec = nn.Sequential(
+            nn.AvgPool2d((height//2**2, width//2**2)), 
+            nn.GELU())
         self.up0 = nn.Sequential(
-            nn.ConvTranspose2d(2 * n_feat, 2 * n_feat, self.h//4, self.h//4), # up-sample  
-            nn.GroupNorm(8, 2 * n_feat), # normalize                       
-            nn.ReLU(),
+            nn.ConvTranspose2d(2*n_feat, 2*n_feat, (height//2**2, width//2**2)),
+            nn.GroupNorm(8, 2*n_feat),
+            nn.GELU()
         )
-        self.up1 = UnetUp(4 * n_feat, n_feat)
-        self.up2 = UnetUp(2 * n_feat, n_feat)
-
-        # Initialize the final convolutional layers to map to the same number of channels as the input image
+        self.up1 = UNetUp(4*n_feat, n_feat)
+        self.up2 = UNetUp(2*n_feat, n_feat)
         self.out = nn.Sequential(
-            nn.Conv2d(2 * n_feat, n_feat, 3, 1, 1), # reduce number of feature maps   #in_channels, out_channels, kernel_size, stride=1, padding=0
-            nn.GroupNorm(8, n_feat), # normalize
-            nn.ReLU(),
-            nn.Conv2d(n_feat, self.in_channels, 3, 1, 1), # map to same number of channels as input
+            nn.Conv2d(2*n_feat, n_feat, 3, 1, 1),
+            nn.GroupNorm(8, n_feat),
+            nn.GELU(),
+            nn.Conv2d(n_feat, in_channels, 1, 1)
         )
+        
+        self.timeemb1 = EmbedFC(1, 2*n_feat)
+        self.timeemb2 = EmbedFC(1, n_feat)
+        self.contextemb1 = EmbedFC(n_cfeat, 2*n_feat)
+        self.contextemb2 = EmbedFC(n_cfeat, n_feat)
 
     def forward(self, x, t, c=None):
-        """
-        x : (batch, n_feat, h, w) : input image
-        t : (batch, n_cfeat)      : time step
-        c : (batch, n_classes)    : context label
-        """
-        # x is the input image, c is the context label, t is the timestep, context_mask says which samples to block the context on
-
-        # pass the input image through the initial convolutional layer
+        if c is None: # unconditional case
+            c = torch.zeros(x.shape[0], self.n_cfeat).to(x.device)
         x = self.init_conv(x)
-        # pass the result through the down-sampling path
-        down1 = self.down1(x)       #[10, 256, 8, 8]
-        down2 = self.down2(down1)   #[10, 256, 4, 4]
-        
-        # convert the feature maps to a vector and apply an activation
-        hiddenvec = self.to_vec(down2)
-        
-        # mask out context if context_mask == 1
-        if c is None:
-            c = torch.zeros(x.shape[0], self.n_cfeat).to(x)
+        down1 = self.down1(x)
+        down2 = self.down2(down1)
+        hidden_vec = self.to_vec(down2)
+        up0 = self.up0(hidden_vec)
+        up1 = self.up1(up0*self.contextemb1(c) + self.timeemb1(t), down2)
+        up2 = self.up2(up1*self.contextemb2(c) + self.timeemb2(t), down1)
+        return self.out(torch.cat([up2, x], axis=1))
+
+
+
+if __name__ == "__main__":
+    x = torch.randn(5, 4, 8, 8)
+    res_conv_block = ResidualConvBlock(4, 8, False)
+    print(x.shape)
+    print(res_conv_block(x).shape)
+
+    unet_down = UNetDown(4, 36)
+    print(x.shape)
+    print(unet_down(x).shape)
+
+    x_skip = torch.randn(5, 4, 8, 8)
+    unet_up = UNetUp(8, 2)
+    print(x.shape)
+    print(unet_up(x, x_skip).shape)
+
+    x_emb = torch.randn(5, 16)
+    embed_fc = EmbedFC(16, 32)
+    print(x_emb.shape)
+    print(embed_fc(x_emb).shape)
+
+    in_channels = 1
+    height = width = 64
+    n_feature = 64
+    n_cfeature = 10
+    context_unet = ContextUnet(in_channels, height, width, n_feature, n_cfeature)
+    
+    timesteps = 100
+    n_samples = 7
+    x = torch.randn(n_samples, in_channels, height, width)
+    c = torch.rand(n_samples, n_cfeature)
+    t = torch.randint(1, timesteps + 1, (n_samples, ))
+    print(x.shape)
+    print(context_unet(x, t/timesteps, c).shape)
             
-        # embed context and timestep
-        cemb1 = self.contextembed1(c).view(-1, self.n_feat * 2, 1, 1)     # (batch, 2*n_feat, 1,1)
-        temb1 = self.timeembed1(t).view(-1, self.n_feat * 2, 1, 1)
-        cemb2 = self.contextembed2(c).view(-1, self.n_feat, 1, 1)
-        temb2 = self.timeembed2(t).view(-1, self.n_feat, 1, 1)
-        #print(f"uunet forward: cemb1 {cemb1.shape}. temb1 {temb1.shape}, cemb2 {cemb2.shape}. temb2 {temb2.shape}")
 
 
-        up1 = self.up0(hiddenvec)
-        up2 = self.up1(cemb1*up1 + temb1, down2)  # add and multiply embeddings
-        up3 = self.up2(cemb2*up2 + temb2, down1)
-        out = self.out(torch.cat((up3, x), 1))
-        return out
+
