33c1f07046dff8fd909a404adafc31c644224a76
burak <byrkbrk@gmail.com>
2024-03-17 19:09:58 +0300

    unpack t_steps variable

--- sample.py
+++ sample.py
@@ -16,11 +16,11 @@ if __name__=="__main__":
     
     train_model = TrainModel(device=args.device, checkpoint_name=args.checkpoint_name)
     c = get_custom_context(n_samples=args.n_samples, n_classes=train_model.nn_model.n_cfeat, device=train_model.device)
-    samples, intermediate_ddpm = train_model.sample_ddpm(args.n_samples, c)
+    samples, intermediate_ddpm, t_steps = train_model.sample_ddpm(args.n_samples, c)
     
     save_image(samples, os.path.join(os.path.dirname(__file__), "generated-images", 
                                     f"{train_model.dataset_name}_ddpm_images.jpeg"), 
                                     scale_each=True, normalize=True, nrow=args.n_images_per_row)
-    generate_animation(intermediate_ddpm, os.path.join(os.path.dirname(__file__), "generated-images",
+    generate_animation(intermediate_ddpm, t_steps, os.path.join(os.path.dirname(__file__), "generated-images",
                                                        f"{train_model.dataset_name}_ani.gif"), 
                                                        n_images_per_row=args.n_images_per_row)
