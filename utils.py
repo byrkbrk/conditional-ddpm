961c6179725fc85d9b618c52caa5681f31eb9756
burak <byrkbrk@gmail.com>
2024-03-19 19:41:27 +0300

    update SpriteDataset

--- utils.py
+++ utils.py
@@ -6,36 +6,20 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os
 
-class CustomDataset(Dataset):
-    def __init__(self, sfilename, lfilename, transform, null_context=False):
+class SpriteDataset(Dataset):
+    def __init__(self, sfilename, lfilename, transform, target_transform):
         self.sprites = np.load(sfilename)
         self.slabels = np.load(lfilename)
-        #print(f"sprite shape: {self.sprites.shape}")
-        #print(f"labels shape: {self.slabels.shape}")
         self.transform = transform
-        self.null_context = null_context
-        self.sprites_shape = self.sprites.shape
-        self.slabel_shape = self.slabels.shape
-                
-    # Return the number of images in the dataset
+        self.target_transform = target_transform
+    
+    def __getitem__(self, idx):
+        image = self.transform(self.sprites[idx])
+        label = self.target_transform(self.slabels[idx])
+        return image, label
+
     def __len__(self):
         return len(self.sprites)
-    
-    # Get the image and label at a given index
-    def __getitem__(self, idx):
-        # Return the image and label as a tuple
-        if self.transform:
-            image = self.transform(self.sprites[idx])
-            if self.null_context:
-                label = torch.tensor(0).to(torch.int64)
-            else:
-                label = torch.tensor(self.slabels[idx]).to(torch.int64)
-        return (image, label)
-
-    def getshapes(self):
-        # return shapes of data and labels
-        return self.sprites_shape, self.slabel_shape
-    
 
 def generate_animation(intermediate_samples, t_steps, fname, n_images_per_row=8):
     intermediate_samples = [make_grid(x, scale_each=True, normalize=True, 
