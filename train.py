b0e2d756fc26775777816169050306f30ad1c5d4
burak <byrkbrk@gmail.com>
2024-03-21 16:28:29 +0300

    update default epochs

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from diffusion_model import DiffusionModel
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Train diffusion model")
     parser.add_argument("--batch-size", type=int, default=128, help="Batch size for training")
-    parser.add_argument("--epochs", type=int, default=32, help="Number of epochs for training")
+    parser.add_argument("--epochs", type=int, default=50, help="Number of epochs for training")
     parser.add_argument("--dataset-name", type=str, default="mnist", help="Dataset name for training")
     parser.add_argument("--device", type=str, default="cuda", help="Device for training")
     parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate for training")
