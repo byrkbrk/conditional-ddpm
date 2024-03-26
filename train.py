30247bdf5c5f217d264ec540dcaa8e5865bf149b
burak <byrkbrk@gmail.com>
2024-03-26 16:17:08 +0300

    add timesteps, beta1, and beta2 arguments

--- train.py
+++ train.py
@@ -9,10 +9,14 @@ if __name__=="__main__":
     parser.add_argument("--device", type=str, default="cuda", help="Device for training")
     parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate for training")
     parser.add_argument("--checkpoint-name", type=str, default=None, help="Checkpoint name for pre-training")
+    parser.add_argument("--timesteps", type=int, default=500, help="Timesteps T for DDPM training")
+    parser.add_argument("--beta1", type=float, default=0.0001, help="Hyperparameter for DDPM")
+    parser.add_argument("--beta2", type=float, default=0.02, help="Hyperparameter for DDPM training")
     args = parser.parse_args()
 
     diffusion_model = DiffusionModel(device=args.device, dataset_name=args.dataset_name, 
                              checkpoint_name=args.checkpoint_name)
-    diffusion_model.train(batch_size=args.batch_size, n_epoch=args.epochs, lr=args.lr)
+    diffusion_model.train(batch_size=args.batch_size, n_epoch=args.epochs, lr=args.lr,
+                          timesteps=args.timesteps, beta1=args.beta1, beta2=args.beta2)
 
 
